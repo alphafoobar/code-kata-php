4f193c0f52663230e0d6818b61f24080ac482b6f
James Little <alphafoobar@users.noreply.github.com>
2019-05-16 22:58:18 +1200

    Added binary search chop kata02

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Code Kata PHP
 
-This implementation is written to PHP 7.1:
+This implementation is written to PHP 7.2:
 * It includes strict types
 * Including `void`
+
+## Using PHP 7.3 on macOS
+* Had to disable JIT: https://stackoverflow.com/a/53709484/7421645
 
 ## To run -
 * Install [PHP](http://php.net/)
@@ -12,3 +15,4 @@ This implementation is written to PHP 7.1:
 ## Running PHPUnit in PHPStorm
 
 [Testing with PHPUnit](https://www.jetbrains.com/help/phpstorm/enabling-php-unit-support.html)
+
--- src/BinarySearch.php
+++ src/BinarySearch.php
@@ -8,7 +8,24 @@ namespace kata02 {
 
         function chop(int $key, array $a): int
         {
-            throw new \RuntimeException("Not implemented yet!");
+            return $this->chop_worker($key, $a, 0, count($a));
+        }
+
+        function chop_worker(int $key, array $a, int $offset, int $length): int
+        {
+            $remaining = $length - $offset;
+            if ($remaining == 0) {
+                return -1;
+            }
+
+            $point = (int)floor($offset + $remaining / 2);
+            if ($a[$point] == $key) {
+                return $point;
+            }
+            if ($a[$point] > $key) {
+                return $this->chop_worker($key, $a, $offset, $point);
+            }
+            return $this->chop_worker($key, $a, $point + 1, $length);
         }
     }
 }
